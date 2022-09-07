1ddb356962d5846299a2a6171ed3bbaf1f69bbb1
Brian Lambert <brianlambert2408@gmail.com>
2022-09-07 10:01:42 +1000

    commit tree 65e2c64760b8e19bb49899e53abadab6e51764cc

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+const dodger = document.querySelector("div#dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+
+document.addEventListener("keydown", (e)=>{
+    // if(e.key == "ArrowUp"){
+
+    // }else if(){}
+
+    switch(e.key){
+        case "ArrowLeft":
+            moveDodgerLeft();
+        break;
+        case "ArrowRight":
+            moveDodgerRight();
+        break;
+    }
+
+});
+
+function moveDodgerLeft(){
+    const left = parseStyle(dodger.style.left);
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const left = parseStyle(dodger.style.left);
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+function parseStyle(sty){
+    const num = sty.replace("px", "");
+    return parseInt(num, 10);
+}
